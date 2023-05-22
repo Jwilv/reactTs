79d59b19d88f366cf1d71fa84c5c5ab468573dc8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-05-22 16:03:31 -0300

    math and useEffect

--- 01-bases/src/Bases/CounterEffect.tsx
+++ 01-bases/src/Bases/CounterEffect.tsx
@@ -1,17 +1,26 @@
-import {useState} from 'react' 
+import { useEffect, useState } from 'react'
 
 export const CouterEffect = () => {
 
-    const [counter, setCounter] = useState( 5 )
+    const [counter, setCounter] = useState(5)
 
-    const handleSuma = ()=>{
-        setCounter( pre => pre + 1  )
+    const MAX_COUNT = 10
+
+    const handleSuma = () => {
+        setCounter(pre => Math.min(pre + 1, MAX_COUNT) )
     }
+
+    useEffect(() => {
+        if (counter < 10 ) return ;
+        console.log("%cEl numero es mayor a 10","color : red")
+    }, [counter])
+
 
     return (
         <>
-        <p>{counter}</p>
-        <button onClick={handleSuma}>+1</button>
+            <h1>effect</h1>
+            <p>{counter}</p>
+            <button onClick={handleSuma}>+1</button>
         </>
     )
 }
