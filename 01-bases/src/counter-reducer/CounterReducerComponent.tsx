4d075f16f8827afb20f41bfd6d9f81f6fc16b0b2
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-01 16:51:47 -0300

    counterReducer

---
+++ 01-bases/src/counter-reducer/CounterReducerComponent.tsx
@@ -0,0 +1,63 @@
+import { useReducer } from 'react'
+
+interface CounterState {
+    counter: number;
+    previous: number;
+    changes: number;
+}
+
+const INITIAL_STATE: CounterState = {
+    counter: 0,
+    previous: 0,
+    changes: 0,
+}
+
+type counterAction =
+    | { type: 'incrementBy', payload: { value: number } }
+    | { type: 'reset' };
+
+const counterReducer = (state: CounterState, action: counterAction): CounterState => {
+
+    switch (action.type) {
+        case 'reset':
+            return {
+                counter: 0,
+                previous: 0,
+                changes: 0,
+            }
+
+        case 'incrementBy':
+            return{
+                ...state,
+                counter : state.counter + action.payload.value,
+                previous : state.counter,
+                changes : state.changes + 1,
+            }
+
+        default:
+            return state;
+    }
+}
+
+export const CounterReducerComponent = () => {
+
+    const [{counter,previous,changes}, dispatch] = useReducer(counterReducer, INITIAL_STATE)
+
+    const handleReset = () => {
+        dispatch({ type: 'reset' })
+    }
+
+    const handleSuma = ()=>{
+        dispatch({type:'incrementBy', payload:{value : 10}})
+    }
+
+    return (
+        <>
+            <p>counter: {counter}</p>
+            <p>chages: {changes}</p>
+            <p>previoous: {previous}</p>
+            <button onClick={handleSuma}>+10</button>
+            <button onClick={handleReset}>reset</button>
+        </>
+    )
+}
