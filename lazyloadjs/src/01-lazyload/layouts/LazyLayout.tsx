cfd27c89b00de353a85ba55c8783ba3437d2889b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-03 18:37:31 -0300

    lazy load

---
+++ lazyloadjs/src/01-lazyload/layouts/LazyLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LazyLayout = () => {
+    return (
+        <h1>LazyLoafing Page </h1>
+    )
+}
+
+export default LazyLayout
--- lazyloadjs/src/routes/routes.ts
+++ lazyloadjs/src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { lazy, LazyExoticComponent } from "react";
 
 
-type JSXElement = ()=> JSX.Element
+type JSXElement = () => JSX.Element
 
 interface Router {
     path: string,
@@ -9,26 +9,26 @@ interface Router {
     name: string,
 }
 
-const LazyPage = lazy( () => import('../01-lazyload/pages/LazyPage'));
-const LazyPage2 = lazy( () => import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy( () => import('../01-lazyload/pages/LazyPage3'));
+// const LazyPage = lazy( () => import('../01-lazyload/pages/LazyPage'));
+// const LazyPage2 = lazy( () => import('../01-lazyload/pages/LazyPage2'));
+// const LazyPage3 = lazy( () => import('../01-lazyload/pages/LazyPage3'));
 
 
 
 export const routes = [
     {
-        path: '/lazy',
-        Component: LazyPage,
-        name: 'lazypage'
+        path: '/lazyload',
+        Component: lazy(() => import('../01-lazyload/layouts/LazyLayout')),
+        name: 'lazyLoading page '
     },
-    {
-        path: '/lazy2',
-        Component: LazyPage2,
-        name: 'lazypage-2'
-    },
-    {
-        path: '/lazy3',
-        Component: LazyPage3,
-        name: 'lazypage-3'
-    },
+    // {
+    //     path: '/lazy2',
+    //     Component: LazyPage2,
+    //     name: 'lazypage-2'
+    // },
+    // {
+    //     path: '/lazy3',
+    //     Component: LazyPage3,
+    //     name: 'lazypage-3'
+    // },
 ]
