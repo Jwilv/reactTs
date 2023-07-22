681896c941f20d51fea9e7028e32460d28990e83
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-07-22 16:10:05 -0300

    validate

--- lazyloadjs/src/03-forms/pages/FormikBasic.tsx
+++ lazyloadjs/src/03-forms/pages/FormikBasic.tsx
@@ -1,9 +1,43 @@
-import { useFormik } from 'formik'
+import { FormikErrors, useFormik } from 'formik'
 
 import React from 'react'
 import "../styles/styles.css"
-import { InitialValues } from '../../02-component-patterns/interfaces/interfaces';
+
+interface FormValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+
 const FormikBasic = () => {
+
+    const validate = ({ firstName, lastName, email }: FormValues ) => {
+        
+        const errors: FormikErrors<FormValues> = {};
+
+        if ( !firstName ) {
+            errors.firstName = 'Required';
+        } else if( firstName.length >= 15 ) {
+            errors.firstName = 'Must be 15 characters or less';
+        }
+
+        
+
+        if ( !lastName ) {
+            errors.lastName = 'Required';
+        } else if( lastName.length >= 10 ) {
+            errors.lastName = 'Must be 10 characters or less';
+        }
+
+        if (!email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test( email )) {
+            errors.email = 'Invalid email address';
+        }
+
+        return errors;
+    }
 
     const { handleChange, values, handleSubmit } = useFormik({
         initialValues: {
@@ -13,7 +47,8 @@ const FormikBasic = () => {
         },
         onSubmit: (values) => {
             console.log(values)
-        }
+        },
+        validate
     })
 
     return (
